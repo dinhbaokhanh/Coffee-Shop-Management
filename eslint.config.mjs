e9c569307f95e94a1378f2cb52aa4cf957109f5a
dinhbaokhanh <khanhdbao@gmail.com>
2025-01-03 00:25:24 +0700

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,36 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+const { FlatCompat } = require("@eslint/eslintrc");
+const js = require("@eslint/js");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-export default compat.config({
-  extends: ["next/core-web-vitals"],
+const compat = new FlatCompat({
+    baseDirectory: __dirname,                  // optional; default: process.cwd()
+    resolvePluginsRelativeTo: __dirname,       // optional
+    recommendedConfig: js.configs.recommended, // optional unless using "eslint:recommended"
+    allConfig: js.configs.all,                 // optional unless using "eslint:all"
 });
+
+module.exports = [
+
+    // mimic ESLintRC-style extends
+    ...compat.extends("standard", "example", "plugin:react/recommended"),
+
+    // mimic environments
+    ...compat.env({
+        es2020: true,
+        node: true
+    }),
+
+    // mimic plugins
+    ...compat.plugins("jsx-a11y", "react"),
+
+    ...compat.config({
+        plugins: ["jsx-a11y", "react"],
+        extends: "standard",
+        env: {
+            es2020: true,
+            node: true
+        },
+        rules: {
+            semi: "error"
+        }
+    })
+];
