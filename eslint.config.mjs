fb198f5d5eea2103f59f7efa2c3b5b80cf8c39a7
dinhbaokhanh <khanhdbao@gmail.com>
2025-01-03 00:20:58 +0700

    menu ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,43 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
+import js from "@eslint/js";
+import path from "path";
 import { fileURLToPath } from "url";
 
+// mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({
+    baseDirectory: __dirname,                  // optional; default: process.cwd()
+    resolvePluginsRelativeTo: __dirname,       // optional
+    recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended"
+    allConfig: js.configs.all,                 // optional unless you're using "eslint:all"
+});
 
-export default {
-  overrides: [
-    {
-      files: ["**/*.js", "**/*.ts"],
-      extends: ["next/core-web-vitals"],
-    },
-  ],
-};
+export default [
+
+    // mimic ESLintRC-style extends
+    ...compat.extends("standard", "example", "plugin:react/recommended"),
+
+    // mimic environments
+    ...compat.env({
+        es2020: true,
+        node: true
+    }),
+
+    // mimic plugins
+    ...compat.plugins("jsx-a11y", "react"),
+
+    // translate an entire config
+    ...compat.config({
+        plugins: ["jsx-a11y", "react"],
+        extends: "standard",
+        env: {
+            es2020: true,
+            node: true
+        },
+        rules: {
+            semi: "error"
+        }
+    })
+];
